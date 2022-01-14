96669a65bc38755252b89fc0251e0be3782f3df4
GRIEGMAK <g.m.ak@yandex.ru>
2022-01-14 22:19:56 +0300

    правки

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mysql = require("mysql2");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 57989;
 
 app.use(express.static("public"));
 app.use(express.json())
